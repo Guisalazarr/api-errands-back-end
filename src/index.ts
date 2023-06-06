624e2a804963f2255fa5a6f26211c90361e93838
Guilherme Salazar <guisalazarr@gmail.com>
2023-06-05 21:15:50 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use('/user', userRoutes());
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
     console.log('API is running...');
