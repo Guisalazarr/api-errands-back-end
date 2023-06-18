88e89418a6166199efd8e00a60577bcfa041ae67
Guilherme Salazar <guisalazarr@gmail.com>
2023-06-18 11:44:03 -0300

    ok

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,4 +1,7 @@
 import { Errand } from '../models/errand.models';
 import { User } from '../models/user.models';
 
-export const users = [];
+export const users = [new User('José', 'jose@teste.com', '1234')];
+
+users[0].errand.push(new Errand('Mercado', 'Fazer compras'));
+users[0].errand.push(new Errand('Lavar', 'Casa de praia'));
