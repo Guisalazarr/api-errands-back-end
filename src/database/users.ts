e1d6fbd86ba7209db9d8dc330cd0ca63252ec2bf
Guilherme Salazar <guisalazarr@gmail.com>
2023-06-18 11:41:17 -0300

    remoção data base

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,16 +1,4 @@
 import { Errand } from '../models/errand.models';
 import { User } from '../models/user.models';
 
-export const users = [
-    new User('José', 'jose@teste.com', '1234'),
-    new User('Pedro', 'pedro@teste.com', '2222'),
-    new User('Maria', 'maria@teste.com', '3333'),
-    new User('Ana', 'ana@teste.com', '5656'),
-    new User('João', 'joao@teste.com', '3311'),
-    new User('Janaina', 'janaina@teste.com', '4321'),
-];
-
-users[0].errand.push(new Errand('Mercado', 'Fazer compras'));
-users[0].errand.push(new Errand('Lavar', 'Casa de praia'));
-users[1].errand.push(new Errand('Cachorro', 'Levar para passear'));
-users[2].errand.push(new Errand('Luz', 'Trocar a lampâda da cozinha'));
+export const users = [];
